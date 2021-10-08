015a22f41711ffe28ae5988d754c588ec9222caf
inam-22 <inamahmed70@@gmail.com>
2021-10-08 14:59:53 +0500

    Assignment#1 Airthematic Operations AI & Chatbot Inam

---
+++ app.js
@@ -0,0 +1,41 @@
+
+function Add(){
+var num1=document.getElementById("num1").value;
+
+var num2=document.getElementById("num2").value;
+
+var add=Number(num1)+Number(num2);
+alert(" The ADDITION Of Given Number Is:"+ " " + add);
+
+}
+
+function Sub(){
+    var num1=document.getElementById("num1").value;
+    
+    var num2=document.getElementById("num2").value;
+    
+    var sub=num1-num2;
+    alert("The SUBTRACTION Of Given Number Is:"+ " " + sub);
+    
+    }
+    
+    function Mul(){
+        var num1=document.getElementById("num1").value;
+        
+        var num2=document.getElementById("num2").value;
+        
+        var mul=num1*num2;
+        alert("The Multiplication Of Given Number Is:"+ " "+ mul);
+        
+        }
+    
+        function Div(){
+            var num1=document.getElementById("num1").value;
+            
+            var num2=document.getElementById("num2").value;
+            
+            var div=num1/num2;
+            alert("The Division Of Given Number Is:"+ " "+ div);
+            
+            }
+            
